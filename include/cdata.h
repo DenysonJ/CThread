c46d7e4742ee84107eca3dc600c6624774cbb97b
Denyson <denysonjurgen@gmail.com>
2016-09-08 05:19:30 -0300

    Creat cthread.c and cthreadfun.h
    A bit of code was made

--- include/cdata.h
+++ include/cdata.h
@@ -8,16 +8,23 @@
 #define __cdata__
 
 #define	PROCST_CRIACAO	0
-#define	PROCST_APTO	1
-#define	PROCST_EXEC	2
-#define	PROCST_BLOQ	3
+#define	PROCST_APTO		1
+#define	PROCST_EXEC		2
+#define	PROCST_BLOQ		3
 #define	PROCST_TERMINO	4
+
+#define TRUE 	1
+#define FALSE 	0
+
+#define ERROR  	 -1
+#define NOTFIRST -2
+#define ERROR_NULL_POINTER -3
 
 /* NÃO ALTERAR ESSA struct */
 typedef struct s_TCB { 
 	int		tid; 		// identificador da thread
 	int		state;		// estado em que a thread se encontra
-					// 0: Criação; 1: Apto; 2: Execução; 3: Bloqueado e 4: Término
+						// 0: Criação; 1: Apto; 2: Execução; 3: Bloqueado e 4: Término
 	int 		ticket;		// "bilhete" de loteria da thread, para uso do escalonador
 	ucontext_t 	context;	// contexto de execução da thread (SP, PC, GPRs e recursos) 
 } TCB_t; 
---
+++ include/cthreadfun.h
@@ -0,0 +1,8 @@
+#ifndef __cthread_fun__
+#define __cthread_fun__
+
+int firstTime();
+int getTicket();
+
+
+#endif
